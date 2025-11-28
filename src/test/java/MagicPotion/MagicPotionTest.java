762660818a0972731e38c00bf43a8b7ec618dc6f
WNCMYN <ewan.Acemyandeoliveira.13@gmail.com>
2025-11-28 17:11:16 +0100

    Tests potionMagic non fini

---
+++ src/test/java/MagicPotion/MagicPotionTest.java
@@ -0,0 +1,47 @@
+package MagicPotion;
+
+import Foods.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+public class MagicPotionTest {
+    MagicPotion potion;
+
+    Mistletoe mistletoe = mock(Mistletoe.class);
+    Carrot carrot = mock(Carrot.class);
+    Salt salt = mock(Salt.class);
+    FourLeaveClover clover = mock(FourLeaveClover.class);
+    Fish fish = mock(Fish.class);
+    StoneOil stoneOil = mock(StoneOil.class);
+    Honey honey = mock(Honey.class);
+    Mead mead = mock(Mead.class);
+    SecretIngredient shhhIngredient = mock(SecretIngredient.class);
+    Foods optionalIngredient = mock(Foods.class);
+    Foods optionalIngredient2 = mock(Foods.class);
+    Foods optionalIngredient3 = mock(Foods.class);
+
+
+    @Test
+    void testConstructeur(){
+        potion = new MagicPotion(mistletoe, carrot, salt, clover, fish, stoneOil, honey, mead, shhhIngredient);
+        when(potion.getEffects()).thenReturn(new ArrayList<>(List.of("force surhumaine", "invincibilité")));
+    }
+
+    void testConstructeur2(){
+        potion = new MagicPotion(mistletoe, carrot, salt, clover, fish, stoneOil, honey, mead, shhhIngredient, optionalIngredient);
+    }
+
+    void testConstructeur3(){
+        potion = new MagicPotion(mistletoe,carrot, salt, clover, fish, stoneOil, honey, mead, shhhIngredient, optionalIngredient, optionalIngredient2);
+    }
+
+    void testConstructeur4(){
+        potion = new MagicPotion(mistletoe,carrot, salt, clover, fish, stoneOil, honey, mead, shhhIngredient, optionalIngredient, optionalIngredient2, optionalIngredient3);
+    }
+}
