5aa85f1f25a9c3ccaef023a3bfa0ebc10c5fb6c9
Thomas Palot <thomas.palot@etu.univ-amu.fr>
2025-12-01 14:30:55 +0100

    Ajout d'attributs et d'un toString dans Place

--- src/main/java/org/places/Place.java
+++ src/main/java/org/places/Place.java
@@ -8,17 +8,18 @@ import java.util.List;
 public abstract class Place {
     private String name;
     private int surface;
+    private ClanLeader chief;
+    private int census;
+    private List<Person> ListOfPeople;
+    private List<Food> ListOfFood;
 
     protected List<Person> people = new ArrayList<>();
-
     public void addPerson(Person person) {
         people.add(person);
     }
-
     public void removePerson(Person person) {
         people.remove(person);
     }
-
     public List<Person> getPeople() {
         return people;
     }
@@ -30,6 +31,20 @@ public abstract class Place {
     }
 
     public void feedSomeone(Person person) {
+
         person.eat();
     }
+
+    @Override
+    public String toString() {
+        return "Nom : " + name + "\n" +
+                "Superficie : " + surface + "\n" +
+                "Chef de clan : " + chief + "\n" +
+                "Recensement : " + census + "\n" +
+                "Liste de personnes : " + ListOfPeople + "\n" +
+                "Liste d'aliments : " + ListOfFood;
+    }
+
+
+
 }
