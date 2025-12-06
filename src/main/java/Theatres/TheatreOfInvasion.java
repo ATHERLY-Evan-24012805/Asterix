959a1f94ec0582df57698247971e5caeb3541864
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2025-12-06 12:00:08 +0100

    ajout du debut de la class TheatreOfInvasion, ne fonctionne pas pour le moment

---
+++ src/main/java/Theatres/TheatreOfInvasion.java
@@ -0,0 +1,25 @@
+package Theatres;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheatreOfInvasion {
+    private String name;
+    private int numberOfPlaces;
+    private List listOfPlaces;
+    private List listOfChief;
+
+    //Constructeur
+    public TheatreOfInvasion(String nameOfTheGame) {
+        this.name = name;
+    }
+
+
+    public List showPlaces() {
+        return listOfPlaces;
+    }
+    public int howManyPlaces() {
+        return listOfPlaces.size();
+    }
+
+}
---
+++ src/main/java/test/TestTheatreOfInvasion.java
@@ -0,0 +1,22 @@
+package test;
+
+import Theatres.TheatreOfInvasion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestTheatreOfInvasion {
+    private TheatreOfInvasion theatre;
+
+/*
+    @BeforeEach
+    void init(){
+        theatre = new TheatreOfInvasion();
+    }
+
+    @Test
+    void testTheatreOfInvasion(){
+
+    }
+
+ */
+}
