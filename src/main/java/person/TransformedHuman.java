1f01deb8f244ca4ec79e13bf8c064e7ce1e69981
WNCMYN <ewan.Acemyandeoliveira.13@gmail.com>
2025-12-11 15:02:34 +0100

    Documentation maitre de zoo fantastique + humain transformé

--- src/main/java/person/TransformedHuman.java
+++ src/main/java/person/TransformedHuman.java
@@ -1,10 +1,32 @@
 package person;
 
+/**
+ * Classe humain transformé après la transformation d'un lycanthrope
+ */
 public class TransformedHuman extends Person {
+    /**
+     * Constructeur de humain transformé
+     * @param name nom
+     * @param gender genre
+     * @param height taille
+     * @param age age
+     * @param strength force
+     * @param endurance endurance
+     */
     public TransformedHuman(String name, char gender, double height, int age, int strength, int endurance) {
         super(name, gender, height, age, strength, endurance);
     }
 
+    /**
+     * Duplication de personnage pour potion magique
+     * @param name nom
+     * @param gender genre
+     * @param height taille
+     * @param age age
+     * @param strength force
+     * @param endurance endurance
+     * @return nouvelle instance de personnage
+     */
     @Override
     public Person duplicate(String name, char gender, double height, int age, int strength, int endurance) {
         return null;
